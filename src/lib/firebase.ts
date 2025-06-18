00cc6f9c358d00f087c52e395c2270905405001c
Sushi <kaopansri@gmail.com>
2025-06-18 23:18:31 +0700

    feat(test-db) : setup db

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,14 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDPH20_SKRIDdhhewUUvLlbhSGot0UxMj8",
-  authDomain: "promptbook-3e8ae.firebaseapp.com",
-  projectId: "promptbook-3e8ae",
-  storageBucket: "promptbook-3e8ae.firebasestorage.app",
-  messagingSenderId: "434608762522",
-  appId: "1:434608762522:web:1192a4e26eee3ea58e7923",
-};
+const firebaseConfig = {};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
