416e86b9d87c9498e6e9942aabcfb9856d70bedc
Yang <pbrfilmm@gmail.com>
2025-06-19 01:54:53 +0700

    feat(homepage): add right  drawer

--- src/components/homepage/HomePage.tsx
+++ src/components/homepage/HomePage.tsx
@@ -4,6 +4,18 @@ import { useEffect, useState } from "react";
 import { onAuthStateChanged, User } from "firebase/auth";
 import { auth } from "@/lib/firebase";
 import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { PanelLeft } from "lucide-react";
 
 export default function HomePage() {
   const [user, setUser] = useState<User | null>(null);
@@ -27,10 +39,31 @@ export default function HomePage() {
 
   return (
     <main className="p-8">
-      <h1 className="text-2xl font-bold mb-2">
-        Welcome, {user.displayName || "User"}!
-      </h1>
-      <p className="text-gray-600">This is your mock dashboard 👋</p>
+      <Sheet>
+        <SheetTrigger asChild>
+          <PanelLeft />
+        </SheetTrigger>
+        <SheetContent side="left">
+          <div className="flex justify-end">
+            <SheetClose asChild>
+              <PanelLeft />
+            </SheetClose>
+          </div>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+      <div>
+        <h1 className="text-2xl font-bold mb-2">
+          Welcome, {user.displayName || "User"}!
+        </h1>
+        <p className="text-gray-600">This is your mock dashboard 👋</p>
+      </div>
     </main>
   );
 }
