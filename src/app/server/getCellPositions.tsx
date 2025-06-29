706ef57cda835730f0ee4a3b4471736714c26b0d
ParisaraCH <parisara.chth@gmail.com>
2025-06-29 18:04:43 +0700

    testing rename, delete, reorder

---
+++ src/app/server/getCellPositions.tsx
@@ -0,0 +1,19 @@
+"use server";
+
+import { PrismaClient } from "../../generated/prisma";
+const prisma = new PrismaClient();
+
+export async function getCellPositions(collectionId: string) {
+  const cells = await prisma.cell.findMany({
+    where: { collectionId },
+    orderBy: { position: "asc" },
+    select: {
+      id: true,
+      position: true,
+      prompt: true,
+      result: true,
+      review: true,
+    },
+  });
+  return cells;
+}
